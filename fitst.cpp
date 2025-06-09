d020362cb7d98c2aeff2ae2df6fccf2efb08115d
harsh23082008 <harshsdv54@gmail.com>
2025-06-09 21:33:34 +0530

    Add files via upload

---
+++ fitst.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+   /*this is for my use only  no body can touch it just go to hell if yout ouch this code just go away from here 
+   and never show me your face you rascal*/
+    cout<<"hello"<<endl<<"world!";
+    return 0;
+}
